b03fc72cb6e8d8f2b37a9c81db36679b4011435f
ellsonmendesyt <elsonmendesoficial@gmail.com>
2021-10-08 16:41:13 -0300

    tela grande

--- src/components/Hero/Hero.styles.ts
+++ src/components/Hero/Hero.styles.ts
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
 import bgright from '../../assets/imgs/hero-img.jpg'
+import bgleft from '../../assets/svgs/curves.svg'
 
 export const Container= styled.div`
-background-color: royalblue;
+/* background-color: royalblue; */
 height: calc(100vh - 106px);
 width:100%;
 
@@ -13,8 +14,21 @@ align-items: stretch;
 `
 
 export const ContentLeft = styled.div`
- background-color: orange;
+ /* background-color: orange; */
  width:50%;
+
+ background-image: url(${bgleft});
+background-position: 0% 120% ;
+background-size: 400px 400px;
+background-repeat: no-repeat;
+
+
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+
+padding: 0px 40px;
 `
 
 export const ContentRight = styled.div`
@@ -24,4 +38,27 @@ flex:1;
 background-image: url(${bgright});
 background-position: center;
 background-size: cover;
+`
+
+
+export const Title= styled.h1`
+ font-size: 56px;
+ line-height: 79px;
+ font-weight: 600;
+`
+
+export const ButtonLarge= styled.button`
+ padding: 15px 40px;
+ border-radius: 30px;
+ background-color: #ffcc00;
+ display: flex;
+ justify-content: center;
+ align-items: center;
+
+ border:none;
+ 
+`
+
+export const Icon= styled.div`
+ margin: 0px 15px;
 `
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
-import {Container,ContentLeft,ContentRight} from '../Hero/Hero.styles'
-
+import {Container,ContentLeft,ContentRight, Title,ButtonLarge,Icon} from '../Hero/Hero.styles'
+import rightArrow from '../../assets/svgs/arro-right.svg'
 
 
 const Hero = () => {
     return (
         <Container>
             <ContentLeft>
-                esquerda
+                <Title >Somos apaixonados por tecnologia, inovação e pessoas.</Title>
+                <ButtonLarge>Saiba mais sobre a maida 
+                    <Icon>
+                    <img src={rightArrow} alt="" />
+                    </Icon>
+                </ButtonLarge>
             </ContentLeft>
             <ContentRight>
               direita
